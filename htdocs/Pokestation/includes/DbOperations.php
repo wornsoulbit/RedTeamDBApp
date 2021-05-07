5768f5fc0fa3e8c137ec03cf01c782893502ffb4
wornsoulbit <alexandervasil840@gmail.com>
2021-05-07 15:43:43 -0400

    finished the API and DbOperations calls

--- htdocs/Pokestation/includes/DbOperations.php
+++ htdocs/Pokestation/includes/DbOperations.php
@@ -1,103 +1,218 @@
-<?php 
+<?php
 
-    class DbOperation {
-        private $connection;
+class DbOperation {
+    private $connection;
 
-        function __construct() {
-            require_once dirname(__FILE__) . '/DbConnect.php';
+    function __construct() {
+        require_once dirname(__FILE__) . '/DbConnect.php';
 
-            $db = new DbConnect();
+        $db = new DbConnect();
 
-            $this->connection = $db->connect();
-        }
-
-        function createIngredient($supplier_id, $order_day, $ingredient_name, $ingredient_type) {
-            $stmt = $this->connection->prepare("INSERT INTO ingredient (supplier_id, order_day, ingredient_name, ingredient_type) 
-                VALUES (?, ?, ?, ?)");
-            $stmt->bind_param("isss", $supplier_id, $order_day, $ingredient_name, $ingredient_type);
-            if ($stmt->execute()) 
-                return true;
-            return false;
-        }
-
-        function getIngredients() {
-            $stmt = $this->connection->prepare("SELECT * FROM ingredient");
-            $stmt->execute();
-            $stmt->bind_result($ingredient_id, $supplier_id, $order_day, $ingredient_name, $ingredient_type);
-
-            $ingredients = array();
-
-            while ($stmt->fetch()) {
-                $ingredient = array();
-                $ingredient['ingredient_id'] = $ingredient_id;
-                $ingredient['supplier_id'] = $supplier_id;
-                $ingredient['order_day'] = $order_day;
-                $ingredient['ingredient_name'] = $ingredient_name;
-                $ingredient['ingredient_type'] = $ingredient_type;
-
-                array_push($ingredients, $ingredient);
-            }
-
-            return $ingredients;
-        }
-
-        function updateIngredient($supplier_id, $order_day, $ingredient_name, $ingredient_type, $ingredient_id) {
-            $stmt = $this->connection->prepare("UPDATE ingredient SET supplier_id = ?, order_day = ?, ingredient_name = ?, ingredient_type = ? WHERE ingredient_id = ?");
-            $stmt->bind_param("isssi", $supplier_id, $order_day, $ingredient_name, $ingredient_type, $ingredient_id);
-            if ($stmt->execute())
-                return true;
-            return false;
-        }
-
-        function deleteIngredient($ingredient_id) {
-            $stmt = $this->connection->prepare("DELETE FROM ingredient WHERE ingredient_id = ?");
-            $stmt->bind_param("i", $ingredient_id);
-            if ($stmt->execute())
-                return true;
-            return false;
-        }
-
-        function createSupplier($supplier_name) {
-            $stmt = $this->connection->prepare("INSERT INTO ingredient (supplier_name) VALUES (?)");
-            $stmt->bind_param("s", $supplier_name);
-            if ($stmt->execute()) 
-                return true;
-            return false;
-        }
-
-        function getSuppliers() {
-            $stmt = $this->connection->prepare("SELECT * FROM supplier");
-            $stmt->execute();
-            $stmt->bind_result($supplier_id, $supplier_name);
-
-            $suppliers = array();
-
-            while ($stmt->fetch()) {
-                $supplier = array();
-                $supplier['supplier_id'] = $supplier_id;
-                $supplier['supplier_name'] = $supplier_name;
-
-                array_push($suppliers, $supplier);
-            }
-
-            return $suppliers;
-        }
-
-        function updateSupplier($supplier_id, $supplier_name) {
-            $stmt = $this->connection->prepare("UPDATE supplier SET supplier_name = ? WHERE supplier_id = ?");
-            $stmt->bind_param("si", $supplier_name, $supplier_id);
-            if ($stmt->execute())
-                return true;
-            return false;
-        }
-
-        function deleteSupplier($supplier_id) {
-            $stmt = $this->connection->prepare("DELETE FROM supplier WHERE supplier_id = ?");
-            $stmt->bind_param("i", $supplier_id);
-            if ($stmt->execute())
-                return true;
-            return false;
-        }
+        $this->connection = $db->connect();
     }
+
+    function createIngredient($supplier_id, $order_day, $ingredient_name, $ingredient_type) {
+        $stmt = $this
+            ->connection
+            ->prepare("INSERT INTO ingredient (supplier_id, order_day, ingredient_name, ingredient_type) 
+                VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("isss", $supplier_id, $order_day, $ingredient_name, $ingredient_type);
+        if ($stmt->execute()) return true;
+        return false;
+    }
+
+    function getIngredients() {
+        $stmt = $this
+            ->connection
+            ->prepare("SELECT * FROM ingredient");
+        $stmt->execute();
+        $stmt->bind_result($ingredient_id, $supplier_id, $order_day, $ingredient_name, $ingredient_type);
+
+        $ingredients = array();
+
+        while ($stmt->fetch()) {
+            $ingredient = array();
+            $ingredient['ingredient_id'] = $ingredient_id;
+            $ingredient['supplier_id'] = $supplier_id;
+            $ingredient['order_day'] = $order_day;
+            $ingredient['ingredient_name'] = $ingredient_name;
+            $ingredient['ingredient_type'] = $ingredient_type;
+
+            array_push($ingredients, $ingredient);
+        }
+
+        return $ingredients;
+    }
+
+    function updateIngredient($ingredient_id, $supplier_id, $order_day, $ingredient_name, $ingredient_type) {
+        $stmt = $this
+            ->connection
+            ->prepare("UPDATE ingredient SET supplier_id = ?, order_day = ?, ingredient_name = ?, ingredient_type = ?
+                WHERE ingredient_id = ?");
+
+        $stmt->bind_param("isssi", $supplier_id, $order_day, $ingredient_name, $ingredient_type, $ingredient_id);
+        if ($stmt->execute()) return true;
+        return false;
+    }
+
+    function deleteIngredient($ingredient_id) {
+        $stmt = $this
+            ->connection
+            ->prepare("DELETE FROM ingredient WHERE ingredient_id = ?");
+        $stmt->bind_param("i", $ingredient_id);
+        if ($stmt->execute()) return true;
+        return false;
+    }
+
+    function createSupplier($supplier_name) {
+        $stmt = $this
+            ->connection
+            ->prepare("INSERT INTO ingredient (supplier_name) VALUES (?)");
+        $stmt->bind_param("s", $supplier_name);
+        if ($stmt->execute()) return true;
+        return false;
+    }
+
+    function getSuppliers() {
+        $stmt = $this
+            ->connection
+            ->prepare("SELECT * FROM supplier");
+        $stmt->execute();
+        $stmt->bind_result($supplier_id, $supplier_name);
+
+        $suppliers = array();
+
+        while ($stmt->fetch()) {
+            $supplier = array();
+            $supplier['supplier_id'] = $supplier_id;
+            $supplier['supplier_name'] = $supplier_name;
+
+            array_push($suppliers, $supplier);
+        }
+
+        return $suppliers;
+    }
+
+    function updateSupplier($supplier_id, $supplier_name) {
+        $stmt = $this
+            ->connection
+            ->prepare("UPDATE supplier SET supplier_name = ? WHERE supplier_id = ?");
+        $stmt->bind_param("si", $supplier_name, $supplier_id);
+        if ($stmt->execute()) return true;
+        return false;
+    }
+
+    function deleteSupplier($supplier_id) {
+        $stmt = $this
+            ->connection
+            ->prepare("DELETE FROM supplier WHERE supplier_id = ?");
+        $stmt->bind_param("i", $supplier_id);
+        if ($stmt->execute()) return true;
+        return false;
+    }
+
+    function createOrder($supplier_id, $ingredient_id, $stock_id, $order_day) {
+        $stmt = $this
+            ->connection
+            ->prepare("INSERT INTO orders (supplier_id, ingredient_id, stock_id, order_day) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("iiis", $supplier_id, $ingredient_id, $stock_id, $order_day);
+        if ($stmt->execute()) return true;
+        return false;
+    }
+
+    function getOrders() {
+        $stmt = $this
+            ->connection
+            ->prepare("SELECT * FROM orders");
+        $stmt->execute();
+        $stmt->bind_result($order_id, $supplier_id, $ingredient_id, $stock_id, $order_day);
+
+        $orders = array();
+
+        while ($stmt->fetch()) {
+            $order = array();
+            $order['order_id'] = $order_id;
+            $order['supplier_id'] = $supplier_id;
+            $order['ingredient_id'] = $ingredient_id;
+            $order['stock_id'] = $stock_id;
+            $order['order_day'] = $order_day;
+
+            array_push($orders, $order);
+        }
+
+        return $orders;
+    }
+
+    function updateOrder($order_id, $supplier_id, $ingredient_id, $stock_id, $order_day) {
+        $stmt = $this
+            ->connection
+            ->prepare("UPDATE orders SET supplier_id = ?, ingredient_id = ?, stock_id = ?, order_day = ?
+                WHERE order_id = ?");
+
+        $stmt->bind_param("isssi", $supplier_id, $ingredient_id, $stock_id, $order_day, $order_id);
+        if ($stmt->execute()) return true;
+        return false;
+    }
+
+    function deleteOrder($order_id) {
+        $stmt = $this
+            ->connection
+            ->prepare("DELETE FROM orders WHERE order_id = ?");
+        $stmt->bind_param("i", $order_id);
+        if ($stmt->execute()) return true;
+        return false;
+    }
+
+    function createStock($ingredient_id, $in_stock, $amount_needed) {
+        $stmt = $this
+            ->connection
+            ->prepare("INSERT INTO stock (ingredient_id, in_stock, amount_needed) VALUES (?, ?, ?)");
+        $stmt->bind_param("iii", $ingredient_id, $in_stock, $amount_needed);
+        if ($stmt->execute()) return true;
+        return false;
+    }
+
+    function getStocks() {
+        $stmt = $this
+            ->connection
+            ->prepare("SELECT * FROM stock");
+        $stmt->execute();
+        $stmt->bind_result($stock_id, $ingredient_id, $in_stock, $amount_needed);
+
+        $orders = array();
+
+        while ($stmt->fetch()) {
+            $order = array();
+            $order['stock_id'] = $stock_id;
+            $order['ingredient_id'] = $ingredient_id;
+            $order['in_stock'] = $in_stock;
+            $order['amount_needed'] = $amount_needed;
+
+            array_push($orders, $order);
+        }
+
+        return $orders;
+    }
+
+    function updateStock($stock_id, $ingredient_id, $in_stock, $amount_needed) {
+        $stmt = $this
+            ->connection
+            ->prepare("UPDATE stock SET ingredient_id = ?, in_stock = ?, amount_needed = ?
+                WHERE stock_id = ?");
+
+        $stmt->bind_param("iiii", $ingredient_id, $in_stock, $amount_needed, $stock_id);
+        if ($stmt->execute()) return true;
+        return false;
+    }
+
+    function deleteStock($stock_id) {
+        $stmt = $this
+            ->connection
+            ->prepare("DELETE FROM stock WHERE stock_id = ?");
+        $stmt->bind_param("i", $stock_id);
+        if ($stmt->execute()) return true;
+        return false;
+    }
+}
 
 ?>
--- htdocs/Pokestation/v1/Api.php
+++ htdocs/Pokestation/v1/Api.php
@@ -27,147 +27,307 @@
 
     if (isset($_GET['apicall'])) {
         switch ($_GET['apicall']) {
-            
+
             case "createSupplier":
-                isTheseParamsAvailable(array("supplier_name"));
+                isTheseParamsAvailable(array(
+                    "supplier_name"
+                ));
 
                 $db = new DbOperation();
-                
+
                 $result = $db->createSupplier($_POST['supplier_name']);
 
                 if ($result) {
                     $response['error'] = false;
                     $response['message'] = "Supplier added successfully";
                     $response['supplier'] = $db->getSuppliers();
-                } else {
+                }
+                else {
                     $response['error'] = true;
                     $response['message'] = "Something went wrong";
                 }
-                break;
+            break;
 
             case "getSuppliers":
                 $db = new DbOperation();
                 $response['error'] = false;
                 $response['message'] = "Request completed";
                 $response['suppliers'] = $db->getSuppliers();
-                break;
+            break;
 
             case "updateSupplier":
-                isTheseParamsAvailable(array("supplier_id", "supplier_name"));
+                isTheseParamsAvailable(array(
+                    "supplier_id",
+                    "supplier_name"
+                ));
                 $db = new DbOperation();
 
-                $result = $db->updateSupplier(
-                    $_POST['supplier_id'],
-                    $_POST['supplier_name']
-                );
+                $result = $db->updateSupplier($_POST['supplier_id'], $_POST['supplier_name']);
 
                 if ($result) {
-                    $response['error'] = false; 
+                    $response['error'] = false;
                     $response['message'] = 'Supplier updated successfully';
                     $response['suppliers'] = $db->getSuppliers();
-                }else{
-                    $response['error'] = true; 
+                }
+                else {
+                    $response['error'] = true;
                     $response['message'] = 'Some error occurred please try again';
                 }
-                    break; 
+            break;
 
-                case "deleteSupplier": 
-                    
-                    if (isset($_GET['supplier_id'])) {
-                        $db = new DbOperation();
+            case "deleteSupplier":
 
-                        if ($db->deleteSupplier($_GET['supplier_id'])) {
-                            $response['error'] = false;
-                            $response['message'] = "Supplier successfully deleted";
-                            $response['supplier'] = $db->getSuppliers();
-                        } else {
-                            $response['error'] = true;
-                            $response['message'] = "Something went wrong";
-                        }
-                    } else {
-                        $response['error'] = true; 
-                        $response['message'] = "Nothing to delete, provide a supplier_id please";
-                    }
+                if (isset($_GET['supplier_id'])) {
+                    $db = new DbOperation();
 
-                    break;
-
-                    case "createIngredient":
-                        isTheseParamsAvailable(array("supplier_id", "order_day", "ingredient_name", "ingredient_type"));
-        
-                        $db = new DbOperation();
-                        
-                        $result = $db->createIngredient(
-                            $_POST['supplier_id'],
-                            $_POST['order_day'],
-                            $_POST['ingredient_name'],
-                            $_POST['ingredient_type']
-                        );
-        
-                        if ($result) {
-                            $response['error'] = false;
-                            $response['message'] = "Ingredient added successfully";
-                            $response['ingredients'] = $db->getIngredients();
-                        } else {
-                            $response['error'] = true;
-                            $response['message'] = "Something went wrong";
-                        }
-                        break;
-        
-                    case "getIngredients":
-                        $db = new DbOperation();
+                    if ($db->deleteSupplier($_GET['supplier_id'])) {
                         $response['error'] = false;
-                        $response['message'] = "Request completed";
-                        $response['ingredients'] = $db->getIngredients();
-                        break;
-        
-                    case "updateIngredient":
-                        isTheseParamsAvailable(array("ingredient_id", "supplier_id", "order_day", "ingredient_name", "ingredient_type"));
-                        $db = new DbOperation();
+                        $response['message'] = "Supplier successfully deleted";
+                        $response['supplier'] = $db->getSuppliers();
+                    }
+                    else {
+                        $response['error'] = true;
+                        $response['message'] = "Something went wrong";
+                    }
+                }
+                else {
+                    $response['error'] = true;
+                    $response['message'] = "Nothing to delete, provide a supplier_id please";
+                }
 
-                        $result = $db->updateIngredient(
-                            $_POST['ingredient_id'],
-                            $_POST['supplier_id'],
-                            $_POST['order_day'],
-                            $_POST['ingredient_name'],
-                            $_POST['ingredient_type']
-                        );
-        
-                        if ($result) {
-                            $response['error'] = false; 
-                            $response['message'] = 'Ingredient updated successfully';
-                            $response['ingredients'] = $db->getIngredients();
-                        }else{
-                            $response['error'] = true; 
-                            $response['message'] = 'Some error occurred please try again';
-                        }
-                            break; 
-        
-                        case "deleteIngredient": 
-                            
-                            if (isset($_GET['ingredient_id'])) {
-                                $db = new DbOperation();
-        
-                                if ($db->deleteIngredient($_GET['ingredient_id'])) {
-                                    $response['error'] = false;
-                                    $response['message'] = "Ingredient successfully deleted";
-                                    $response['ingredients'] = $db->getIngredients();
-                                } else {
-                                    $response['error'] = true;
-                                    $response['message'] = "Something went wrong";
-                                }
-                            } else {
-                                $response['error'] = true; 
-                                $response['message'] = "Nothing to delete, provide a ingredient_id please";
-                            }
-        
-                            break;
+            break;
+
+            case "createIngredient":
+                isTheseParamsAvailable(array(
+                    "supplier_id",
+                    "order_day",
+                    "ingredient_name",
+                    "ingredient_type"
+                ));
+
+                $db = new DbOperation();
+
+                $result = $db->createIngredient($_POST['supplier_id'], $_POST['order_day'], $_POST['ingredient_name'], $_POST['ingredient_type']);
+
+                if ($result) {
+                    $response['error'] = false;
+                    $response['message'] = "Ingredient added successfully";
+                    $response['ingredients'] = $db->getIngredients();
+                }
+                else {
+                    $response['error'] = true;
+                    $response['message'] = "Something went wrong";
+                }
+            break;
+
+            case "getIngredients":
+                $db = new DbOperation();
+                $response['error'] = false;
+                $response['message'] = "Request completed";
+                $response['ingredients'] = $db->getIngredients();
+            break;
+
+            case "updateIngredient":
+                isTheseParamsAvailable(array(
+                    "ingredient_id",
+                    "supplier_id",
+                    "order_day",
+                    "ingredient_name",
+                    "ingredient_type"
+                ));
+                // var_dump($_POST);
+                $db = new DbOperation();
+
+                $result = $db->updateIngredient($_POST['ingredient_id'], $_POST['supplier_id'], $_POST['order_day'], $_POST['ingredient_name'], $_POST['ingredient_type']);
+
+                if ($result) {
+                    $response['error'] = false;
+                    $response['message'] = 'Ingredient updated successfully';
+                    $response['ingredients'] = $db->getIngredients();
+                }
+                else {
+                    $response['error'] = true;
+                    $response['message'] = 'Some error occurred please try again';
+                }
+            break;
+
+            case "deleteIngredient":
+
+                if (isset($_GET['ingredient_id'])) {
+                    $db = new DbOperation();
+
+                    if ($db->deleteIngredient($_GET['ingredient_id'])) {
+                        $response['error'] = false;
+                        $response['message'] = "Ingredient successfully deleted";
+                        $response['ingredients'] = $db->getIngredients();
+                    }
+                    else {
+                        $response['error'] = true;
+                        $response['message'] = "Something went wrong";
+                    }
+                }
+                else {
+                    $response['error'] = true;
+                    $response['message'] = "Nothing to delete, provide a ingredient_id please";
+                }
+
+            break;
+
+            case "createOrder":
+                isTheseParamsAvailable(array(
+                    "supplier_id",
+                    "ingredient_id",
+                    "stock_id",
+                    "order_day"
+                ));
+
+                $db = new DbOperation();
+
+                $result = $db->createOrder($_POST['supplier_id'], $_POST['ingredient_id'], $_POST['stock_id'], $_POST['order_day']);
+
+                if ($result) {
+                    $response['error'] = false;
+                    $response['message'] = "Order added successfully";
+                    $response['order'] = $db->getOrders();
+                }
+                else {
+                    $response['error'] = true;
+                    $response['message'] = "Something went wrong";
+                }
+            break;
+
+            case "getOrders":
+                $db = new DbOperation();
+                $response['error'] = false;
+                $response['message'] = "Request completed";
+                $response['order'] = $db->getOrders();
+            break;
+
+            case "updateOrder":
+                isTheseParamsAvailable(array(
+                    "order_id",
+                    "supplier_id",
+                    "ingredient_id",
+                    "stock_id",
+                    "order_day"
+                ));
+                $db = new DbOperation();
+
+                $result = $db->updateOrder($_POST['order_id'], $_POST['supplier_id'], $_POST['ingredient_id'], $_POST['stock_id'], $_POST['order_day']);
+
+                if ($result) {
+                    $response['error'] = false;
+                    $response['message'] = 'Order updated successfully';
+                    $response['order'] = $db->getOrders();
+                }
+                else {
+                    $response['error'] = true;
+                    $response['message'] = 'Some error occurred please try again';
+                }
+            break;
+
+            case "deleteOrders":
+
+                if (isset($_GET['order_id'])) {
+                    $db = new DbOperation();
+
+                    if ($db->deleteOrder($_GET['order_id'])) {
+                        $response['error'] = false;
+                        $response['message'] = "Order successfully deleted";
+                        $response['order'] = $db->getOrders();
+                    }
+                    else {
+                        $response['error'] = true;
+                        $response['message'] = "Something went wrong";
+                    }
+                }
+                else {
+                    $response['error'] = true;
+                    $response['message'] = "Nothing to delete, provide a order_id please";
+                }
+
+            break;
+
+            case "createStock":
+                isTheseParamsAvailable(array(
+                    "ingredient_id",
+                    "in_stock",
+                    "amount_needed"
+                ));
+
+                $db = new DbOperation();
+
+                $result = $db->createStock($_POST['ingredient_id'], $_POST['in_stock'], $_POST['amount_needed']);
+
+                if ($result) {
+                    $response['error'] = false;
+                    $response['message'] = "Stock added successfully";
+                    $response['order'] = $db->getStocks();
+                }
+                else {
+                    $response['error'] = true;
+                    $response['message'] = "Something went wrong";
+                }
+            break;
+
+            case "getStocks":
+                $db = new DbOperation();
+                $response['error'] = false;
+                $response['message'] = "Request completed";
+                $response['order'] = $db->getStocks();
+            break;
+
+            case "updateStock":
+                isTheseParamsAvailable(array(
+                    "stock_id",
+                    "ingredient_id",
+                    "in_stock",
+                    "amount_needed"
+                ));
+                $db = new DbOperation();
+
+                $result = $db->updateStock($_POST['stock_id'], $_POST['ingredient_id'], $_POST['in_stock'], $_POST['amount_needed']);
+
+                if ($result) {
+                    $response['error'] = false;
+                    $response['message'] = 'Stock updated successfully';
+                    $response['order'] = $db->getStocks();
+                }
+                else {
+                    $response['error'] = true;
+                    $response['message'] = 'Some error occurred please try again';
+                }
+            break;
+
+            case "deleteStock":
+
+                if (isset($_GET['stock_id'])) {
+                    $db = new DbOperation();
+
+                    if ($db->deleteOrder($_GET['stock_id'])) {
+                        $response['error'] = false;
+                        $response['message'] = "Stock successfully deleted";
+                        $response['order'] = $db->getStocks();
+                    }
+                    else {
+                        $response['error'] = true;
+                        $response['message'] = "Something went wrong";
+                    }
+                }
+                else {
+                    $response['error'] = true;
+                    $response['message'] = "Nothing to delete, provide a stock_id please";
+                }
+
+            break;
         }
 
-    } else {
+    }
+    else {
         $response['error'] = true;
         $response['message'] = "Invalid API Call";
     }
 
     echo json_encode($response);
-    
+
 ?>
