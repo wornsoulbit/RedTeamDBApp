dc6c2729f6e51104cc4dd57b42cb53d59e25526f
MinhT1 <MinhThienTran1@hotmail.com>
2021-05-14 19:30:38 -0400

    Created the layout for Suppliers and Orders.

---
+++ PokestationApp/app/src/main/java/com/example/pokestationapp/Orders_add.java
@@ -0,0 +1,14 @@
+package com.example.pokestationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Orders_add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orders_add);
+    }
+}
