bc583beb7ec1e0710f40b2a4cc4dae884e193b33
domenicospol <doms1580@gmail.com>
2021-05-16 16:50:39 -0400

    Made 'add company' button functional CompaniesActivity.java.

--- PokestationApp/app/src/main/java/com/example/pokestationapp/CompaniesActivity.java
+++ PokestationApp/app/src/main/java/com/example/pokestationapp/CompaniesActivity.java
@@ -2,13 +2,21 @@ package com.example.pokestationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CompaniesActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_companies);
+    }
+
+    public void addCompany(View view) {
+        Intent intent = new Intent(this, Suppliers_Add.class);
+        startActivity(intent);
     }
 }
