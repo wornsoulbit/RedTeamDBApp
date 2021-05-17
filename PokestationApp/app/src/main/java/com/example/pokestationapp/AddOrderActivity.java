bb5fa64e70430eea600b709a155a6e59736c61c0
domenicospol <doms1580@gmail.com>
2021-05-16 20:58:37 -0400

    Removed previous commit and a few changes.

--- PokestationApp/app/src/main/java/com/example/pokestationapp/AddOrderActivity.java
+++ PokestationApp/app/src/main/java/com/example/pokestationapp/AddOrderActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
@@ -21,4 +22,8 @@ public class AddOrderActivity extends AppCompatActivity {
 
     }
 
+    public void chooseDate(View view) {
+        Intent intent = new Intent(this, Orders_Main.class);
+        startActivity(intent);
+    }
 }
