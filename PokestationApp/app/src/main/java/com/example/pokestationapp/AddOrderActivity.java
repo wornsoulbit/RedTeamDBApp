ad9c828f27f885ba63ef6009605af694c872fa99
domenicospol <doms1580@gmail.com>
2021-05-16 20:40:36 -0400

    Added feature to select a date for an order.

--- PokestationApp/app/src/main/java/com/example/pokestationapp/AddOrderActivity.java
+++ PokestationApp/app/src/main/java/com/example/pokestationapp/AddOrderActivity.java
@@ -1,14 +1,41 @@
 package com.example.pokestationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-public class AddOrderActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class AddOrderActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_order);
+
+    }
+
+    public void showDatepicker(View view) {
+        DialogFragment datepicker = new DatePickerFragment();
+        datepicker.show(getSupportFragmentManager(), "Date picker");
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        String currentDateString = DateFormat.
+                getDateInstance(DateFormat.FULL).format(c.getTime());
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText(currentDateString);
     }
 }
